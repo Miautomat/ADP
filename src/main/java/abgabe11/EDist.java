d514acc2c1d6357e484fa0fb2d0c67ed48b16133
PeiPei15 <PeiPeiGo95@gmail.com>
2016-12-08 23:17:30 +0100

    Edist kommentiert und getestet

--- src/main/java/abgabe11/EDist.java
+++ src/main/java/abgabe11/EDist.java
@@ -7,45 +7,57 @@ package abgabe11;
  */
 class EDist {
     static int editDistDP(String str1, String str2, int m, int n) {
-        int distanceMatrix[][] = new int[m + 1][n + 1];
+        int distMatrix[][] = new int[m + 1][n + 1];
+        // jew +1, damit die Reihe mit index 0 auch dabei ist!
+        // Matrix muss nur bis gewünschten Punkt erstellt werden
         
-        // bottom up
+        /*
+         * bottom up --- i = index for str1(column) --- j = index for str2
+         * (line)
+         */
         for (int i = 0; i <= m; i++) {
             for (int j = 0; j <= n; j++) {
-                // If first string is empty, only option is to
-                // insert all characters of second string
+                /*
+                 * sobald i oder j 0 sind, gehen wir bei einer der beiden Seiten
+                 * von einem leeren String aus. Somit ist die Editierdistanz
+                 * immer i bzw. j bzw. entspricht genau der Anzahl der bisher
+                 * betrachteten Buchstaben
+                 */
                 if (i == 0) {
-                    distanceMatrix[i][j] = j;// Min. operations = j
+                    distMatrix[i][j] = j;
+                } else if (j == 0) {
+                    distMatrix[i][j] = i;
                 }
                 
-                // If second string is empty, only option is to
-                // remove all characters of second string
-                else if (j == 0) {
-                    distanceMatrix[i][j] = i;// Min. operations = i
-                }
-                
-                // If last characters are same, ignore last char
-                // and recur for remaining string
+                /*
+                 * Falls die betrachteten Buchstaben gleich sind, erhöht sich
+                 * die editierdistanz von dem feld nicht und wird vom vorigen
+                 * übertragen
+                 */
                 else if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
-                    distanceMatrix[i][j] = distanceMatrix[i - 1][j - 1];
+                    distMatrix[i][j] = distMatrix[i - 1][j - 1];
                 }
-                // If last character are different, consider all
-                // possibilities and find minimum
+                /*
+                 * Falls sie sich unterscheiden, wird die Editierdistanz um ein
+                 * erhöht und die minimale Distanz der umliegenden Distanzen
+                 * addiert
+                 */
                 else {
-                    distanceMatrix[i][j] = 1 + minimalDistance(distanceMatrix[i][j - 1], // Insert
-                        distanceMatrix[i - 1][j], // Remove
-                        distanceMatrix[i - 1][j - 1]); // Replace
+                    distMatrix[i][j] = 1 + minimalDistance(distMatrix[i][j - 1],
+                        distMatrix[i - 1][j],
+                        distMatrix[i - 1][j - 1]);
                 }
             }
         }
-        return distanceMatrix[m][n];
+        return distMatrix[m][n];
     }
     
     static int minimalDistance(int x, int y, int z) {
-        if (x < y && x < z) // x is minimum
-            return x;
-        if (y < x && y < z) // y is mimimum
-            return y;
+        // muss einmal <= abprüfen falls x = y
+        if (x <= y && x < z)
+            return x;// x or y is minimum
+        if (y < x && y < z)
+            return y; // y is mimimum
         else
             return z; // z is minimum
     }
--- src/test/java/abgabe11/EDistTest.java
+++ src/test/java/abgabe11/EDistTest.java
@@ -1,17 +1,34 @@
 package abgabe11;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class EDistTest {
+    String s1, s2, s3, s4;
     
     @Before
     public void setUp() throws Exception {
+        s1 = "apfel";
+        s2 = "pferd";
     }
     
     @Test
-    public void test() {
-
+    public void testEditDistDP() {
+        assertEquals(1, EDist.editDistDP(s1, s2, 1, 1));
+        assertEquals(5, EDist.editDistDP(s1, s2, 2, 5));
+        assertEquals(3, EDist.editDistDP(s1, s2, 5, 5));
+        assertEquals(0, EDist.editDistDP(s1, s2, 0, 0));
+    }
+    
+    @Test
+    public void testMinimalDistance() {
+        assertEquals(1, EDist.minimalDistance(1, 2, 3));
+        assertEquals(1, EDist.minimalDistance(3, 1, 2));
+        assertEquals(1, EDist.minimalDistance(3, 2, 1));
+        assertEquals(1, EDist.minimalDistance(1, 1, 2)); // überprüfen!
+        assertEquals(1, EDist.minimalDistance(1, 1, 1));
     }
     
 }
