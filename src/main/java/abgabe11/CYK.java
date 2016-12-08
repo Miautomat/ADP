ce58c1add8e1222193f988fcf4969df07ef68136
lukahartwig <luka.hartwig@haw-hamburg.de>
2016-12-08 20:14:47 +0100

    CYK finished

--- src/main/java/abgabe11/CYK.java
+++ src/main/java/abgabe11/CYK.java
@@ -1,6 +1,7 @@
 package abgabe11;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -8,85 +9,139 @@ import java.util.Set;
 import abgabe08.Grammar;
 import abgabe08.Rule;
 
+/**
+ * @author Mieke Narjes, David Hoeck, Luka Hartwig
+ *     <p>Implementation of the CYK-Algorithm
+ */
 public class CYK {
-    
-    private Grammar grammar;
-    private String word;
-    private List<Set<String>> table;
-    
-    public CYK(Grammar grammar, String word) {
-        // TODO falls toChomsky nicht funktioniert einfach weg lassen
-        this.table = new ArrayList<Set<String>>();
-        this.grammar = grammar.toChomsky(false);
-        if (word.toLowerCase().matches("[a-z]*")) {
-            this.word = word.toLowerCase();
-        } else {
-            throw new IllegalArgumentException("Illegal Argument: " + word.toLowerCase());
-        }
 
-      for (int m = 0; m < word.length() * word.length(); m++) {
-        table.add(new HashSet<String>());
-      }
-        
+  private Grammar grammar;
+  private String word;
+  private List<Set<String>> table;
+
+  /**
+   * Precondition: grammar has to be in CNF
+   *
+   * @param grammar grammar in CNF
+   * @param word word
+   */
+  public CYK(Grammar grammar, String word) {
+    this.table = new ArrayList<>();
+    this.grammar = grammar;
+    if (word.toLowerCase().matches("[a-z]*")) {
+      this.word = word.toLowerCase();
+    } else {
+      throw new IllegalArgumentException("Illegal Argument: " + word.toLowerCase());
     }
 
+    for (int m = 0; m < word.length() * word.length(); m++) {
+      table.add(new HashSet<String>());
+    }
+  }
+
+  /**
+   * Checks if the grammar can produce the word
+   *
+   * @return true if the word is accepted, flase otherwise
+   */
   public boolean compute() {
     init();
     for (int row = 1; row < word.length() + 1; row++) {
       for (int col = 0; col < word.length() - row; col++) {
         Set<String> result = new HashSet<>();
         for (int k = 0; k < row; k++) {
-          System.out.println("s1 - col: " + col + " k + col: " + (k + col));
-          System.out
-                  .println("s2 - row + col: " + (row + col) + " col + k:" + (col + k + 1));
 
-          System.out.println(toString());
+          int[] cell1 = new int[] {col, k + col};
+          int[] cell2 =
+              new int[] {
+                col + k + 1, row + col,
+              };
 
-          result.addAll(
-                  integrateAll(atIndex(col, k + col), atIndex(row + col, col + k + 1)));
+          result.addAll(integrateAll(atIndex(cell1[0], cell1[1]), atIndex(cell2[0], cell2[1])));
           add(col, col + row, result);
+          System.out.println(grammar.toString());
+          System.out.println(word);
+          System.out.println(
+              "Step: "
+                  + row
+                  + " cell1="
+                  + Arrays.toString(cell1)
+                  + " cell2="
+                  + Arrays.toString(cell2)
+                  + " => "
+                  + result.toString());
+          System.out.println(toString());
         }
       }
     }
     return atIndex(0, word.length() - 1).contains(grammar.getStartsymbol());
   }
-    
-    private void init() {
-        for (int i = 0; i < word.length(); i++) {
-            String substring = Character.toString(word.charAt(i));
-            add(i, i, integrate(substring));
-        }
+
+  /** Initializes the table with the word as diagonal */
+  private void init() {
+    for (int i = 0; i < word.length(); i++) {
+      String substring = Character.toString(word.charAt(i));
+      add(i, i, integrate(substring));
     }
-    
-    private Set<String> integrateAll(Set<String> s1, Set<String> s2) {
-        Set<String> result = new HashSet<>();
-        
-        for (String elem1 : s1) {
-            for (String elem2 : s2) {
-                result.addAll(integrate(elem1 + elem2));
-            }
-        }
-        return result;
+  }
+
+  /**
+   * Takes two sets of nonterminals and returns a set of nonterminals that can produce atleast one
+   * combination of elements of set one with set two
+   *
+   * @param s1 set of first nonterminals
+   * @param s2 set of second nonterminals
+   * @return set of nonterminals
+   */
+  private Set<String> integrateAll(Set<String> s1, Set<String> s2) {
+    Set<String> result = new HashSet<>();
+
+    for (String elem1 : s1) {
+      for (String elem2 : s2) {
+        result.addAll(integrate(elem1 + elem2));
+      }
     }
-    
-    private Set<String> integrate(String word) {
-        Set<String> result = new HashSet<>();
-        Set<Rule> rules = grammar.getRules();
-        for (Rule rule : rules) {
-            if (rule.getRight().contains(word)) {
-                result.add(rule.getLeft());
-            }
-        }
-        return result;
+    return result;
+  }
+
+  /**
+   * Returns a set of nonterminals that can produce the given symbol
+   *
+   * @param symbol symbol that might be produced
+   * @return set of producers
+   */
+  private Set<String> integrate(String symbol) {
+    Set<String> result = new HashSet<>();
+    Set<Rule> rules = grammar.getRules();
+    for (Rule rule : rules) {
+      if (rule.getRight().contains(symbol)) {
+        result.add(rule.getLeft());
+      }
     }
-    
-    private Set<String> atIndex(int i, int j) {
-        return table.get(j * word.length() + i);
-    }
-    
-    private void add(int i, int j, Set<String> element) {
-        table.add(j * word.length() + i, element);
-    }
+    return result;
+  }
+
+  /**
+   * Gets the element at a given position in the table
+   *
+   * @param i column
+   * @param j row
+   * @return element
+   */
+  private Set<String> atIndex(int i, int j) {
+    return table.get(j * word.length() + i);
+  }
+
+  /**
+   * Adds an element to the given position in the table
+   *
+   * @param i column
+   * @param j row
+   * @param element element
+   */
+  private void add(int i, int j, Set<String> element) {
+    table.set(j * word.length() + i, element);
+  }
 
   @Override
   public String toString() {
@@ -97,6 +152,7 @@ public class CYK {
       }
       string.append(String.format("%-10s", table.get(i).toString()));
     }
+    string.append("\n");
     return string.toString();
   }
 }
--- src/test/java/abgabe11/CYKTest.java
+++ src/test/java/abgabe11/CYKTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import abgabe08.Grammar;
 import abgabe08.Rule;
 
+import static org.junit.Assert.assertTrue;
+
 public class CYKTest {
 
   private Grammar grammar;
@@ -14,19 +16,24 @@ public class CYKTest {
   @Before
   public void setUp() throws Exception {
     grammar = new Grammar("S", "e");
-    grammar.addRule(new Rule("S", "A"));
-    grammar.addRule(new Rule("S", "aB"));
-    grammar.addRule(new Rule("S", "a"));
-    grammar.addRule(new Rule("S", "SA"));
-    grammar.addRule(new Rule("S", "B"));
+    grammar.addRule(new Rule("S", "AB"));
+    grammar.addRule(new Rule("S", "CD"));
+    grammar.addRule(new Rule("S", "AT"));
+    grammar.addRule(new Rule("S", "CU"));
+    grammar.addRule(new Rule("S", "SS"));
+    grammar.addRule(new Rule("T", "SB"));
+    grammar.addRule(new Rule("U", "SD"));
+    grammar.addRule(new Rule("A", "a"));
     grammar.addRule(new Rule("B", "b"));
+    grammar.addRule(new Rule("C", "c"));
+    grammar.addRule(new Rule("D", "d"));
 
-    cyk = new CYK(grammar, "aabbaaa");
+    cyk = new CYK(grammar, "abcabd");
   }
 
   @Test
   public void compute() throws Exception {
-    cyk.compute();
+    assertTrue(cyk.compute());
   }
 
 }
